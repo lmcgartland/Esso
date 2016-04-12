d4f0193cd1e71f67c95cd73fef4b6ea3c77a9730
Nathan Wallace <techietotoro@gmail.com>
2016-04-12 15:31:25 -0700

    Continues implementation

--- Esso/ViewController.swift
+++ Esso/ViewController.swift
@@ -7,13 +7,27 @@
 //
 
 import Cocoa
+import Quartz
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var pdfView: PDFView!
+    @IBOutlet weak var pdfThumbnailView: PDFThumbnailView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let pdfDoc = PDFDocument(URL: NSURL(fileURLWithPath: "/Users/Nathan/Downloads/310 Project 1 Assignment Details.pdf"))
+        pdfView.setDocument(pdfDoc)
+        
+        pdfView.setBackgroundColor(NSColor.clearColor())
+        pdfView.setDisplayBox(kPDFDisplayBoxMediaBox)
+        pdfView.setDisplaysPageBreaks(false)
+        pdfView.setAutoScales(true)
+        
+        pdfThumbnailView.setBackgroundColor(NSColor.clearColor())
     }
 
     override var representedObject: AnyObject? {
