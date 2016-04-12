41ad1971331528dfeaf8379699114f6af3e5cb27
lmcgartland <luke.mcgartland@gmail.com>
2016-04-12 14:12:02 -0700

    Initial Commit

---
+++ Esso/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Esso
+//
+//  Created by Luke McGartland on 4/12/16.
+//  Copyright © 2016 Luke McGartland. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
